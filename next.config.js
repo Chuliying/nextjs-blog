fa79f32d0bb1c45e4a8c9d45665e2e3ab0f98409
Chuliying <chuliyinn@gmail.com>
2023-11-21 21:39:36 +0800

    no message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     "output": "export",
+    images: {
+        domains: ['https://chuliying.github.io/nextjs-blog/'],
+      },
 }
 
 module.exports = nextConfig
