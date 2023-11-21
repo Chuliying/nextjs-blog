0cd89311585efc4001297abef0035676f8708b49
Chuliying <chuliyinn@gmail.com>
2023-11-21 22:11:19 +0800

    no message

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
     "output": "export",
     basePath: '/nextjs-blog',
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/nextjs-blog',
+                basePath: false,
+                permanent: false
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
