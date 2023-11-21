27a07c71cb2a321420f1e4a45215932853278a67
Chuliying <chuliyinn@gmail.com>
2023-11-21 22:55:35 +0800

    no message

--- loader.js
+++ loader.js
@@ -1,5 +1,6 @@
 'use client'
  
-export default function myImageLoader(src) {
-  return `https://chuliying.github.io/nextjs-blog/${src}}`
+export default function myImageLoader(image) {
+  console.log(image.src)
+  return `https://chuliying.github.io/nextjs-blog${image.src}}`
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ const goals: SixGoals[] = [
 ];
 
 export default function Home() {
+
   return (
     <main className='flex min-h-screen flex-col items-center'>
       {/* KV */}
