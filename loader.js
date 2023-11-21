77bf16f93d0d523e3f44c206e713f3cb998ee503
Chuliying <chuliyinn@gmail.com>
2023-11-21 23:37:17 +0800

    no message

--- loader.js
+++ loader.js
@@ -2,5 +2,5 @@
  
 export default function myImageLoader(image) {
   console.log(image.src)
-  return `https://chuliying.github.io/nextjs-blog${image.src}}`
+  return `https://chuliying.github.io/nextjs-blog${image.src}`
 }
