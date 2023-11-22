cebaa954dafb1e5d1b963bee54218ddea16656c4
Chuliying <chuliyinn@gmail.com>
2023-11-22 09:02:40 +0800

    no message

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV = 'production'
 const nextConfig = {
-    output: "standalone",
-    reactStrictMode: true,
+    "output": "export",
+    // basePath: isProd ? '/nextjs-blog' : '',
 
     images: {
       loader: 'akamai',
